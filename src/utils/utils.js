3e152f46ab2b2b63b27b15589119b4b14d3f5f5a
Mahesh Kumar Kumawat <pot30270@posten.local>
2019-05-05 11:41:46 +0200

    removed utils folder

--- src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export const debounce = (func, delay) => {
-    var inDebounce
-    return function() {
-      const context = this
-      const args = arguments
-      clearTimeout(inDebounce)
-      inDebounce = setTimeout(() => func.apply(context, args), delay)
-    }
-}
